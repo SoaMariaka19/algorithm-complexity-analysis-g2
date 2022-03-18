d02c954e6537c47585191e0542c9e07d3a49b0c3
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-03-18 09:09:09 +0300

    update: adding answer

--- 14-count-sum-of-two-representations.js
+++ 14-count-sum-of-two-representations.js
@@ -2,12 +2,25 @@
  * Given integers n, l and r, find the number of ways to represent n as a sum of two integers A and B such that l ≤ A ≤ B ≤ r.
  */
 const countSumOfTwoRepresentations = (n, l, r) => {
-    let result = 0;
-    for (let i = 1; i <= r; i++) {
-        let a = n - i;
-        if (a >= 1 && a <= r && a >= i) {
-            result++;
+    let result = 0; // 1
+    for (let i = 1; i <= r; i++) { // 6
+        let a = n - i; // 4
+        if (a >= 1 && a <= r && a >= i) { // 8
+            result++; // 2
         }
     }
-    return result;
+    return result; // 1
 };
+
+/*
+    5 => 1
+    6 => 6
+    7 => 4
+    8 => 8
+    9 => 2
+    12 => 1
+
+    T1(n) = 1 + 6r(4 + 8 + 2) + 1 = 1 + r (1) + 1*
+
+    O(n) = n => complexité linéaire
+ */
