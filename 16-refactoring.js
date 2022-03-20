44d7c1a982c37aadf612c25c7a797c4a8b0d5fdd
Ny Hasinavalona Randriantsarafara <50411096+nyhasina@users.noreply.github.com>
2022-03-20 23:46:12 +0300

    Revert "feat : complexity change of 16-refactoring.js"

--- 16-refactoring.js
+++ 16-refactoring.js
@@ -1,15 +1,12 @@
 /**
- * Compute the sum of an array of number by finding its max value and removing it
+ * Compute the sum of an array of numbers
  * @param {array} numbers
  * @returns {number}
  */
 const total = (numbers) => {
     let sum = 0;
-    const n=numbers.length
-    for (let i = 0; i < n; i++) {
-        let max=Math.max(...numbers)
-        sum+=max;
-        numbers.splice(numbers.indexOf(max),1)
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
     }
     return sum;
 }
