7afc4697876c2cfd445eff6abb36ac822a234c5a
Soa Mariaka RAKOTOMALALA <hei.soa.2@gmail.com>
2022-03-22 19:31:41 +0300

    Revert "feat: calcul exo10"
    This reverts commit c74851cba80f39cfd86687b6241701817cc25d16.

--- 10-line-encoding.js
+++ 10-line-encoding.js
@@ -9,27 +9,19 @@
  */
 
 const lineEncoding = (s) => {
-  let count = 1;                                               //1
-  let ans = "";                                                //1
-  for (let i = 0; i < s.length; i++) {                         //5
-    if (s[i] === s[i + 1]) {                                   //8
-      count++;                                                 //1
+  let count = 1;
+  let ans = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      count++;
     } else {
-      if (count > 1) {                                         //3
-        ans += count + s[i];                                   //7
-      } else {     
-        ans += s[i];                                           //5
+      if (count > 1) {
+        ans += count + s[i];
+      } else {
+        ans += s[i];
       }
-      count = 1;                                               //1
+      count = 1;
     }
   }
-  return ans;                                                  //1
+  return ans;
 };
-/**
- * T_10(i) = 1 + 1 + 5i(8+1+3+7+5+1) + 1
- * T_10(i) = 3 + 100i
- * T_10(i) = 1 + i
- * T_10(i) = 1 + n
- * O(n)
- * Linéaire
- */
